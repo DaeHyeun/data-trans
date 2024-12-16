d923c75b2552fa00d900e827baa38272d2d25bec
DaeHyeun <dh1236@naver.com>
2024-12-16 10:48:39 +0900

    aa

--- src/main/java/com/example/data_trans/DataTransApplication.java
+++ src/main/java/com/example/data_trans/DataTransApplication.java
@@ -3,11 +3,23 @@ package com.example.data_trans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class DataTransApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataTransApplication.class, args);
+		// 첫 번째 애플리케이션 포트 8080 설정
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("server.port", 8080);
+
+		SpringApplication app = new SpringApplication(Main.class);
+		app.setDefaultProperties(properties);
+		app.run(args);
+
+		System.out.println("사용자1 서버 실행 - 포트 8080");
 	}
 
 }
