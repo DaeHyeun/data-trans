58360442bf7b6350d893759efce5208cb8acdd26
DaeHyeun <dh1236@naver.com>
2024-12-17 13:58:59 +0900

    ã초기파일 ì재세팅

--- src/main/java/com/example/data_trans/DataTransApplication.java
+++ src/main/java/com/example/data_trans/DataTransApplication.java
@@ -11,15 +11,6 @@ public class DataTransApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataTransApplication.class, args);
-		// 첫 번째 애플리케이션 포트 8080 설정
-		Map<String, Object> properties = new HashMap<>();
-		properties.put("server.port", 8080);
-
-		SpringApplication app = new SpringApplication(Main.class);
-		app.setDefaultProperties(properties);
-		app.run(args);
-
-		System.out.println("사용자1 서버 실행 - 포트 8080");
 	}
 
 }
